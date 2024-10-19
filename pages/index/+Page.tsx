dc5eac89406a35b373190cbe11ed37c51faea909
lindejacob <43791907+lindejacob@users.noreply.github.com>
2024-10-19 17:37:34 +0200

    Finished about me page

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -11,6 +11,7 @@ import { Card } from "../../components/card/Card";
 import arrowDown from "../../assets/arrowDown.svg";
 import fabledCard from "../../assets/fabledCard.svg";
 import curlyHighlightArrow from "../../assets/curlyHighlightArrow.svg";
+import profilePicture from "../../assets/TempPic.png";
 
 export default function Page() {
    return (
@@ -88,21 +89,35 @@ export default function Page() {
          </section>
          <section className={styles.aboutMeSection}>
             <div className={styles.contentContainer}>
-               <div className={styles.nameContainer}>
-                  <h3>Who is</h3>
-                  <h2>Jacob Mølholt Poulsen</h2>
+               <div className={styles.infoContainer}>
+                  <div className={styles.nameContainer}>
+                     <h3>Who is</h3>
+                     <h2>Jacob Mølholt Poulsen</h2>
+                  </div>
+                  <div className={styles.studyContainer}>
+                     <h3>Studying</h3>
+                     <h2>Software engineering</h2>
+                  </div>
+                  <p className={styles.aboutText}>
+                     Lorem ipsum dolor sit amet consectetur. Pharetra fringilla
+                     velit scelerisque rhoncus. Pulvinar vitae nisi ultrices
+                     amet cras integer proin commodo. Eget placerat adipiscing
+                     egestas risus nisi ultrices suscipit massa molestie. Odio
+                     velit non risus morbi pretium leo.
+                  </p>
                </div>
-               <div className={styles.studyContainer}>
-                  <h3>Studying</h3>
-                  <h2>Software engineering</h2>
+               <div className={styles.profileImgContainer}>
+                  <img src={profilePicture} alt="Picture of Jacob" />
                </div>
-               <p className={styles.aboutText}>
-                  Lorem ipsum dolor sit amet consectetur. Pharetra fringilla
-                  velit scelerisque rhoncus. Pulvinar vitae nisi ultrices amet
-                  cras integer proin commodo. Eget placerat adipiscing egestas
-                  risus nisi ultrices suscipit massa molestie. Odio velit non
-                  risus morbi pretium leo.
-               </p>
+            </div>
+            <div className={styles.skillsBtnContainer}>
+               <p>Skills</p>
+               <CircleButton
+                  imageUrl={arrowDown}
+                  imageSize={"50%"}
+                  backgroundColor={"var(--highlight-color"}
+                  size={"3rem"}
+               />
             </div>
          </section>
       </main>
